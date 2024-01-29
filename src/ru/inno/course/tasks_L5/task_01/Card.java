dc1d056e3413fe115b9dafd5abedb47ba3672e76
Badgrif <aswd@asd.com>
2024-01-29 21:00:20 +0300

    fixes

--- src/ru/inno/course/tasks_L5/task_01/Card.java
+++ src/ru/inno/course/tasks_L5/task_01/Card.java
@@ -3,10 +3,10 @@ package ru.inno.course.tasks_L5.task_01;
 public class Card {
     private String id;
     private int expirationDate;
-    private int cvv;
-    private int pin;
+    private String cvv;
+    private String pin;
 
-    public Card(String id, int expirationDate, int cvv, int pin) {
+    public Card(String id, int expirationDate, String cvv, String pin) {
         this.id = id;
         this.expirationDate = expirationDate;
         this.cvv = cvv;
@@ -17,7 +17,7 @@ public class Card {
         System.out.println("**** **** **** " + id.substring(id.length() - 4));
     }
 
-    public void checkingPin(int pin) {
+    public void checkingPin(String pin) {
         if (pin == this.pin) {
             System.out.println(this.id);
         } else {
